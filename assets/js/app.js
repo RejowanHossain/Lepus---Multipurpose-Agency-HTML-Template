c1eecbdaf4fba1bb2e6f1cce0e4466b39fcc5650
RejowanHossain <h.rezwan@outlook.com>
2023-02-18 18:41:51 +0600

    finifshed home 2

--- assets/js/app.js
+++ assets/js/app.js
@@ -126,6 +126,68 @@
         });
 
         /*---------------------------------------------------
+            home 2 brand carousel
+        ----------------------------------------------------*/
+
+        $('.logo-slider').slick({
+            dots: false,
+            infinite: true,
+            arrows: true,
+            prevArrow: "",
+            nextArrow: "",
+            autoplay: true,
+            autoplaySpeed: 2000,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            responsive: [{
+                breakpoint: 1060,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 950,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 700,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+
+        /*---------------------------------------------------
             portfolio slider
         ----------------------------------------------------*/
         $('.portfolio-slider').slick({
